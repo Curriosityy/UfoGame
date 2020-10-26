e255d89083c6cc09724575d00360e122174d13c9
Patryk Basa <patryk9595@gmail.com>
2020-10-26 13:15:27 +0100

    -init commit

---
+++ Source/UfoShooter/UfoShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UfoShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UFOSHOOTER_API AUfoShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
